a7c8de6c5b1df1f661a5858dae79f6ef49ed5dec
tracer-nju <tracer@nju.edu.cn>
2022-08-08 00:02:58 +0800

    Linux archepc 5.18.16-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 03 Aug 2022 11:25:04 +0000 x86_64 GNU/Linux  00:02:58 up 12:54,  1 user,  load average: 1.00, 0.81, 0.64

--- libco/co.c
+++ libco/co.c
@@ -1,8 +1,41 @@
 #include "co.h"
 #include <stdlib.h>
+#include <stdint.h>
+#include <setjmp.h>
+
+#define STACK_SIZE 8192
+
+enum co_status{
+  CO_NEW = 1,
+  CO_RUNNING,
+  CO_WAITING,
+  CO_DEAD
+};
 
 struct co {
+  char *name;
+  void (*func)(void *);
+  void *arg;
+
+  enum co_status status;
+  struct co *waiter;
+  jmp_buf context;
+  uint8_t stack[STACK_SIZE];  
 };
+
+struct co* current;
+
+static inline void stack_switch_call(void *sp,void *entry,uintptr_t arg){
+  asm volatile (
+    #if __x86_64__
+      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+        : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
+    #else
+      "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+        : : "b"((uintptr_t)sp-4), "d"(entry), "a"(arg) : "memory"
+    #endif
+  );
+}
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   return NULL;
