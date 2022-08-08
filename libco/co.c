eca7d875fdd398b7bf768832e4229961426d2582
tracer-nju <tracer@nju.edu.cn>
2022-08-08 21:37:35 +0800

    Linux archepc 5.18.16-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 03 Aug 2022 11:25:04 +0000 x86_64 GNU/Linux  21:37:35 up  6:06,  1 user,  load average: 0.36, 0.27, 0.22

--- libco/co.c
+++ libco/co.c
@@ -30,7 +30,7 @@ typedef struct co  {
   enum co_status status;
   struct co *waiter;
   jmp_buf context;
-  
+
   uint8_t stack[STACK_SIZE] __attribute__((aligned(16)));
 } co_t;
 
@@ -50,11 +50,11 @@ __col_t *co_head,*co_tail;
 static inline void stack_switch_call(void *sp,void *entry,uintptr_t arg){
   asm volatile (
     #if __x86_64__
-      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
-        : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
+        "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+          : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
     #else
-      "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
-        : : "b"((uintptr_t)sp-8), "d"(entry), "a"(arg) : "memory"
+        "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+          : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
     #endif
   );
 }
@@ -72,7 +72,7 @@ static inline void __co_resume(co_t *co){
   co_current = co;
   if(co->status == CO_NEW){
     co->status = CO_RUNNING;
-    stack_switch_call(co->stack+STACK_SIZE,co->func,(uintptr_t)co->arg);
+    stack_switch_call(co->stack+STACK_SIZE-8,co->func,(uintptr_t)co->arg);
   }else{
     longjmp(co->context,0);
   }
