da5985529b1d4616239cc0089bff90647a2f4716
tracer-nju <tracer@nju.edu.cn>
2022-08-21 22:19:13 +0800

    Linux archepc 5.19.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 17 Aug 2022 13:48:51 +0000 x86_64 GNU/Linux  22:19:13 up  6:17,  4 users,  load average: 0.61, 0.79, 0.70

--- libco/co.c
+++ libco/co.c
@@ -127,7 +127,7 @@ static inline co_t *__co_list_fetch(){
   return ret;
 }
 
-__attribute__((noinline))
+__attribute__((optimize("O0")))
 static void __co_resume(co_t *co){
 
   assert(co->status != CO_DEAD);
