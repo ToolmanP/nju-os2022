2742e3a7e99abddcbe3ecd3600aac03fe08c1839
tracer-nju <tracer@nju.edu.cn>
2022-09-09 01:40:21 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:40:21 up 25 min,  0 users,  load average: 0.09, 0.04, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -22,28 +22,31 @@ assert(0);\
 #define TODO()
 #define FAILURE()
 #endif
-#define MAXCMDLEN 4096
-#define MAXARGS 1024
-#define MAXSYSCALLNAME 256
+#define MAXCMDLEN 1024
+#define MAXARGS 128
+#define MAXSYSCALLNAME 64
 #define MAXGROUPS 3
+
 typedef struct node{
   char syscall[MAXSYSCALLNAME];
   double duration;
-  TAILQ_ENTRY(node) nodes;
+  SLIST_ENTRY(node) field;
 } node_t;
-typedef TAILQ_HEAD(head,node) head_t;
+
+typedef SLIST_HEAD(head,node) head_t;
 
 static struct timeval timeout = {
   .tv_sec = 1,
   .tv_usec = 0
 };
+
 static char exec_cmd[MAXCMDLEN];
 static char *exec_argv[MAXCMDLEN];
 static char tmp[MAXCMDLEN];
 static char buf[MAXCMDLEN];
 
-static char *regex_extract(char *str,regmatch_t *regMatch){
-  static char buf[MAXSYSCALLNAME];
+static inline char *regex_extract(char *str,regmatch_t *regMatch){
+  static char buf[MAXSYSCALLNAME] = {0};
   char tmp = *(str+regMatch->rm_eo);
   *(str+regMatch->rm_eo) = 0;
   strcpy(buf,(str+regMatch->rm_so));
@@ -51,14 +54,30 @@ static char *regex_extract(char *str,regmatch_t *regMatch){
   return buf;
 }
 
+static inline void syscall_list_insert(head_t *hd,char *csys, double dur){
+  node_t *elm;
+  SLIST_FOREACH(elm,hd,field){
+    if(strcmp(elm->syscall,csys)==0){
+      elm->duration+=dur;
+      return;
+    }
+  }
+  elm = malloc(sizeof(node_t));
+  elm->duration = dur;
+  strcpy(elm->syscall,csys);
+  SLIST_INSERT_HEAD(hd,elm,field);
+}
+
 int main(int argc, char *argv[], char *envp[])
 { 
 
   int pid,pipes[2],reti,i;
-  char *line,*ppath;
+  char *line,*ppath,*rtmp;
   char **pexec_arg,**parg;
   char tmp[MAXCMDLEN];
-  char *rtmp;
+  double duration;
+  
+  head_t *hd;
   size_t maxlen;
   ssize_t nreads;
   regex_t regexCompiled;
@@ -80,13 +99,14 @@ int main(int argc, char *argv[], char *envp[])
   line = NULL;
   for(pexec_arg=exec_argv+2,parg=argv+1;*parg;pexec_arg++,parg++)
     *pexec_arg=*parg;
-
   *pexec_arg = "2>/dev/null";
-
   if(reti = regcomp(&regexCompiled,"([^(]*)\\(.*\\)\\s*=\\s-??[0-9a-fx]*\\s[^<]*<([.0-9]*)>",REG_EXTENDED)){
     printf("Regex Compilaton Error\n");
     exit(EXIT_FAILURE);
   }
+
+  hd = malloc(sizeof(head_t));
+  SLIST_INIT(hd);
 
   if((pid = fork()) == 0){
     close(pipes[0]);
@@ -100,14 +120,16 @@ int main(int argc, char *argv[], char *envp[])
     assert(0);
   }else{
     close(pipes[1]);
+
     while((nreads = getline(&line,&maxlen,in)) != -1){
       if(regexec(&regexCompiled,line,MAXGROUPS,matchGroups,0) == 0){
-        rtmp = regex_extract(line,&matchGroups[1]);
-        printf("%s\n",rtmp);
         rtmp = regex_extract(line,&matchGroups[2]);
-        printf("%s\n",rtmp);
+        duration = atof(rtmp);
+        rtmp = regex_extract(line,&matchGroups[1]);
+        syscall_list_insert(hd,rtmp,duration);
       }
     }
   }
+  
   return 0;
 }
