9856e3710c082e4649a8c6ca8a1345ace7e9a8a4
tracer-nju <tracer@nju.edu.cn>
2022-09-08 21:23:19 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:19 up  1:01,  0 users,  load average: 0.01, 0.01, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
 #include <unistd.h>
 #include <assert.h>
+#include <malloc.h>
+#include <regex.h>
+#include <sys/queue.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <time.h>
 
 #ifdef LOCAL_MACHINE
 #define TODO()\
@@ -11,24 +16,52 @@ do {\
 printf("This section is waiting you to finish\n");\
 assert(0);\
 }while(0)
+#define FAILURE() assert(0);
 #else
 #define TODO()
+#define FAILURE()
 #endif
 #define MAXCMDLEN 4096
+
+typedef struct node{
+  char cmd[MAXCMDLEN];
+  float duration;
+  TAILQ_ENTRY(node) nodes;
+} node_t;
+
+typedef TAILQ_HEAD(head,node) head_t;
 
 static int flides[2];
 static char *PATH = NULL;
 static char cmd[MAXCMDLEN];
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[], char *envp[])
+{
+
+  int pid,fildes[2];
+  char *PATH,*token;
+  float tot;
 
   assert(argc>=2);
+
+  if(pipe(fildes)>0)
+    assert(0);
+  
   PATH = getenv("PATH");
-  char *token = strtok(PATH,":");
-  while(token){
-    sprintf(cmd,"%s/%s",token,argv[1]);
-    printf("%s\n",cmd);
-    token = strtok(NULL,":");
+  tot = 0;
+  pid = fork();
+
+  if(pid == 0){
+    dup2(fildes[1],STDERR_FILENO);
+    token = strtok(PATH,":");
+    while(token){
+      sprintf(cmd,"%s/%s",token,argv[1]);
+      token = strtok(NULL,":");
+    }
+    execve(cmd,argv+1,envp);
+    assert(0);
+  }else{
+    wait(NULL);
   }
   // assert(argc>=2);
   // pipe(flides);
