d81bc2eba050d33787ead0e62a45db138dd4df74
tracer-nju <tracer@nju.edu.cn>
2022-09-08 15:16:56 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:16:56 up  3:49,  0 users,  load average: 0.00, 0.00, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -16,7 +16,7 @@ assert(0)\
 
 int main(int argc, char *argv[]) {
   char *exec_argv[] = { "strace", "ls", NULL, };
-  char *exec_envp[] = { "PATH=/bin", NULL, };
+  char *exec_envp[] = { "PATH=/usr/bin", NULL, };
   
   execve("strace",          exec_argv, exec_envp);
   // execve("/bin/strace",     exec_argv, exec_envp);
