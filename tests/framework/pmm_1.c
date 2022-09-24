8d3523a1e5f24583dccfaccf2494168cc5aded7f
tracer-nju <tracer@nju.edu.cn>
2022-09-24 19:24:53 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:24:53 up  2:50,  0 users,  load average: 0.00, 0.00, 0.00

--- tests/framework/pmm_1.c
+++ tests/framework/pmm_1.c
@@ -11,7 +11,7 @@ int _pmm_nbits_round(size_t size);
 static inline void test()
 {
   printf("%d\n",_pmm_nbits_round(4096));
-  void *ptr = pmm->alloc(20);
+  void *ptr = pmm->alloc(1);
   printf("%p\n",ptr);
 }
 
