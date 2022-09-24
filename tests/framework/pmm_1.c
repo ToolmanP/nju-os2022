362e06a46bbedc3bf1791c89f55a016bc04ecd57
tracer-nju <tracer@nju.edu.cn>
2022-09-24 20:30:44 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:30:44 up  3:56,  0 users,  load average: 0.07, 0.02, 0.00

--- tests/framework/pmm_1.c
+++ tests/framework/pmm_1.c
@@ -14,6 +14,9 @@ static inline void test()
   void *ptr1 = pmm->alloc(1);
   void *ptr2 = pmm->alloc(1);
   void *ptr3 = pmm->alloc(2);
+
+  pmm->free(ptr1);
+  ptr1 = pmm->alloc(1);
   printf("%p %p %p\n",ptr1,ptr2,ptr3);
 }
 
