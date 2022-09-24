56bfe4281a1236cd6a33dbf5cd242683813580e3
tracer-nju <tracer@nju.edu.cn>
2022-09-24 22:08:29 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:08:29 up  5:34,  0 users,  load average: 0.00, 0.00, 0.00

--- tests/framework/pmm_single.c
+++ tests/framework/pmm_single.c
@@ -1,30 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
 
 #include <sys/queue.h>
 #include <time.h>
 #include <common.h>
 #include <klib.h>
+#include <malloc.h>
 
-
-typedef struct _list {
+typedef struct _node {
     void *ptr;
-    SLIST_ENTRY(_list);
-} list_t;
+    SLIST_ENTRY(_node) field;
+} node_t;
 
-typedef SLIST_HEAD(_list_head,_list) head_t;
+typedef SLIST_HEAD(_head,_node) head_t;
 
-static __always_inline inline void single_thread_stress_test(int ntimes)
+head_t *alloc_hd;
+int len;
+
+static inline void *op_alloc(int sz)
 {
+    void *ptr = pmm->alloc(sz);
+    node_t *elm = malloc(sizeof(node_t));
+    elm->ptr = ptr;
+    len++;
+    SLIST_INSERT_HEAD(alloc_hd,elm,field);
+    return ptr;
+}
+
+static inline void *op_free()
+{   
+    if(len == 0)
+        return (void *)-1;
+
+    int pos = rand()%len;
+    node_t *elm;
+    SLIST_FOREACH(elm,alloc_hd,field){
+        if(pos--==0)
+            break;
+    }
+    SLIST_REMOVE(alloc_hd,elm,_node,field);
+    pmm->free(elm->ptr);
+    return elm->ptr;
+}
+
+static __always_inline void single_thread_stress_test(int ntimes)
+{   
+    void *ptr;
+    while(ntimes--){
+        if((rand() % 2) == 0){
+            printf("A,%p\n",op_alloc((rand() % MALLOCMAX)+1));
+        }else{
+            ptr = op_free();
+            if(ptr == (void *)-1)
+                continue;
+            else
+                printf("F,%p\n",ptr);
+        } 
+    }
     
 }
 
 int main(const char *args)
 {   
     int ntimes = atoi(args);
-    printf("%d\n",ntimes);
+    alloc_hd = malloc(sizeof(head_t));
+    SLIST_INIT(alloc_hd);
     srand(time(NULL));
     os->init();
     single_thread_stress_test(ntimes);
