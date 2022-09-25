4b8bce5ba5a0cd2c581bd3c50c984803b7f3fc39
tracer-nju <tracer@nju.edu.cn>
2022-09-25 22:35:49 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:35:49 up 11:05,  0 users,  load average: 0.00, 0.03, 0.00

--- tests/framework/pmm/smoke.c
+++ tests/framework/pmm/smoke.c
@@ -11,7 +11,8 @@ static inline void smoke_test()
 {
   void *ptr1 = pmm->alloc(2048);
   void *ptr2 = pmm->alloc(2048);
-  printf("%p %p\n",ptr1,ptr2);
+  void *ptr3 = pmm->alloc(2048);
+  printf("%p %p %p\n",ptr1,ptr2,ptr3);
 }
 
 int main() {
