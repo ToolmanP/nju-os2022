2f7e251236caa64e82b4ae0f38d8dcf838e30a20
tracer-nju <tracer@nju.edu.cn>
2022-09-25 17:42:20 +0800

    Linux epc 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:42:20 up  6:12,  0 users,  load average: 0.09, 0.03, 0.01

--- tests/framework/pmm/single.c
+++ tests/framework/pmm/single.c
@@ -69,7 +69,7 @@ static void single_thread_stress_test(int ntimes,int mode)
     while(ntimes--){
         if((rand() % 2) == 0){
             sz = generate_size(mode);
-            printf("A,%p\n",op_alloc(128));
+            printf("A,%p\n",op_alloc(256));
         }else{
             ptr = op_free();
             if(ptr == (void *)-1)
