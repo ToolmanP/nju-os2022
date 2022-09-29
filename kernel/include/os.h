b3651f6ffc22b51ed3471b9094ce93d98f0c32e3
toolmanp <toolmanp@outlook.com>
2022-09-29 17:22:27 +0800

    Feat: Add kmt api to os struct

--- kernel/include/os.h
+++ kernel/include/os.h
@@ -1,10 +1,15 @@
 #include <common.h>
+#include <common/queue.h>
 
-struct task {
-  const char *name;
-  void (*entry)(void *arg);
-  void *arg;
-  
+#define STACKSIZE 8192
+#define IRQ_MIN 0
+#define IRQ_MAX INT32_MAX
+
+enum{
+  READY = 0,
+  RUNNING,
+  WAITING,
+  DEAD,
 };
 
 struct spinlock {
@@ -12,8 +17,47 @@ struct spinlock {
   int locked;
 };
 
+struct task {
+    const char *name;
+    void (*entry)(void *arg);
+    void *arg;
+    Area stack;
+    Context *context;
+};
+
+typedef struct thread{
+  task_t *task;
+  int status;
+} thread_t;
+
+NODE_TYPEDEF(task_t *,task);
+NODE_TYPEDEF(thread_t *,thread);
+LIST_TYPEDEF(task);
+LIST_TYPEDEF(thread);
+QUEUE_TYPEDEF(task);
+QUEUE_TYPEDEF(thread);
+
+typedef struct process{
+  LIST_T(thread) *prthreads;
+  LIST_T(thread) *pwthreads;
+  int pid;
+  int status;
+} process_t;
+
+
+NODE_TYPEDEF(process_t *,process);
+LIST_TYPEDEF(process);
+QUEUE_TYPEDEF(process);
+
+typedef struct cpu{
+  thread_list_t *kthreads;
+  spinlock_t lock;
+  int cpuid;
+} cpu_t;
+
 struct semaphore {
   const char *name;
+  thread_queue_t *wthreads;
+  spinlock_t lock;
   int value;
-  struct spinlock lock;
 };
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,11 +1,136 @@
 #include <common.h>
 
-static void os_init() {
-  pmm->init();
-  kmt->init();
+#include <modules/kmt.h>
+#include <os.h>
+#define MAXIRQS 64
+
+typedef struct irq{
+  int seq,event;
+  handler_t handler;
+} irq_t;
+
+typedef struct irqlist{
+  irq_t irqs[MAXIRQS];
+  int nirqs;
+}irqlist_t;
+
+irqlist_t irql;
+
+static void irq_swap(irq_t *a, irq_t *b)
+{
+  irq_t tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;  
 }
 
-static void os_run() {
+static void irq_sort(irq_t *irqs,int l,int r) // Quicksort why not ?
+{ 
+  if(l>=r) return;
+  irq_t tmp = irqs[l];
+  int i = l;
+  int j = r;
+  while(i<j){
+      while(irqs[j].seq >= tmp.seq) j--;
+      if(i>=j) break;
+      irq_swap(&irqs[i],&irqs[j]);
+      while(irqs[i].seq < tmp.seq) i++;
+      if(i>=j) break;
+      irq_swap(&irqs[i],&irqs[j]);
+  }
+  irqs[i] = tmp;
+  irq_sort(irqs,l,i-1);
+  irq_sort(irqs,i+1,r);
+}
+
+static int context_check(Context *ctx)
+{
+#if __ARCH__ == x86_64-qemu
+  return 0;
+#elif
+
+#else
+
+#endif
+  return -1;
+}
+
+static Context *os_context_save_irq_handler(Event ev, Context *ctx)
+{
+  kmt_context_save(ev,ctx);
+  return ctx;
+}
+
+static Context *os_context_switch_irq_handler(Event ev, Context *ctx)
+{
+  return kmt_context_switch(ev,ctx);
+}
+
+static Context *os_syscall_irq_handler(Event ev,Context *ctx)
+{
+  return (void *)-1;
+}
+
+static Context *os_pagefault_irq_handler(Event ev,Context *ctx)
+{
+  return (void *)-1;
+}
+
+static Context *os_timer_irq_handler(Event ev, Context *ctx)
+{
+  return (void *)-1;
+}
+
+static Context *os_devio_irq_handler(Event ev, Context *ctx)
+{
+  return (void *)-1;
+}
+
+static Context *os_error_irq_handler(Event ev, Context *ctx)
+{
+  panic("CPU Error Happened!");
+  return NULL;
+}
+
+static Context *os_trap(Event ev, Context *ctx)
+{
+  Context *next = NULL;
+  for(int i=0;i<irql.nirqs;i++){
+    if(irql.irqs[i].event == EVENT_NULL || ev.event == irql.irqs[i].event){
+      next = irql.irqs[i].handler(ev,ctx);
+      if(next) break;
+    }
+  }
+  panic_on(next == (void *)-1, "Not Implemented irq handler");
+  panic_on(next == NULL, "Returning null context");
+  panic_on(context_check(next) != 0, "Returning invalid context");
+  return next;
+
+}
+
+static void os_irq(int seq, int event, handler_t handler)
+{
+  irql.irqs[irql.nirqs++].seq = seq;
+  irql.irqs[irql.nirqs].handler = handler;
+  irql.irqs[irql.nirqs].event = event;
+}
+
+static void os_init()
+{
+  pmm->init();
+  kmt->init();
+  irq_sort(irql.irqs,0,irql.nirqs-1);
+  os_irq(IRQ_MIN,EVENT_NULL,os_context_save_irq_handler);
+  os_irq(300,EVENT_IRQ_IODEV,os_devio_irq_handler);
+  os_irq(200,EVENT_IRQ_TIMER,os_timer_irq_handler);
+  os_irq(100,EVENT_SYSCALL,os_syscall_irq_handler);
+  os_irq(400,EVENT_PAGEFAULT,os_pagefault_irq_handler);
+  os_irq(500,EVENT_ERROR,os_error_irq_handler); 
+  os_irq(IRQ_MAX,EVENT_NULL,os_context_switch_irq_handler);
+}
+
+static void os_run()
+{
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
@@ -15,4 +140,6 @@ static void os_run() {
 MODULE_DEF(os) = {
   .init = os_init,
   .run  = os_run,
+  .trap = os_trap,
+  .on_irq = os_irq
 };
